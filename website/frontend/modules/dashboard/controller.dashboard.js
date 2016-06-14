32a768808839d24feff27007b090f53a0d5958b6
Austin Anderson <aanders50@gmail.com>
2016-06-14 16:47:30 -0400

    more splash page work

--- website/frontend/modules/dashboard/controller.dashboard.js
+++ website/frontend/modules/dashboard/controller.dashboard.js
@@ -2,40 +2,37 @@
 
 angular.module('Dashboard', ['ngMaterial', 'ngRoute', 'chart.js'])
 
-.controller('DashboardController', ['$scope','$location','$timeout','$routeParams','$http','$route','$mdToast',
-  function($scope,$location,$timeout,$routeParams,$http,$route,$mdToast) {
-
-
-    //will run when event queue for this controller has settled down
+.controller('DashboardController', ['$scope','$timeout',
+  function($scope,$timeout) {
 
     $scope.isViewLoading = true;
     $scope.search = search;
     $scope.user = {
-      name: ''
+      name: '',
+      region: ''
     };
     $scope.loadingText = 'Champion Mastery is Loading...';
+    $scope.regions = [{name: 'br'},{name: 'eune'},{name: 'euw'},{name: 'kr'},{name: 'lan'},{name: 'las'},{name: 'na'},{name: 'oce'},{name: 'ru'},{name: 'tr' }]
+    
     activate();
 
     function activate() {
-      //showStep1();
-      $timeout(function() {
-      $('.loader').hide();
-      $('.step-1').show();
-    },1000);
+      showStep1();
     }
 
     function showStep1() {
+      $scope.user.region = $scope.regions[6].name;
       $timeout(function() {
-        $('.loader').fadeOut(1500,function(){
-          $('.step-1').fadeIn(1500);
+        $('.loader').fadeOut(1000,function(){
+          $('.step-1').fadeIn(1000);
         });
-      }, 1500);
+      }, 1000);
     }
 
     function search() {
       $scope.loadingText = 'Searching for Summoner...';
-      $('.step-1').fadeOut(1500,function(){
-        $('.loader').fadeIn(1500);
+      $('.step-1').fadeOut(1000,function(){
+        $('.loader').fadeIn(1000);
       });
     }
 
